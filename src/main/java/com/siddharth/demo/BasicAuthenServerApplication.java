e6039b8cbfab0fce168c4247edf461d5ca9b137d
Licious <Licious@Admins-MacBook-Pro.local>
2021-10-01 16:21:35 +0530

    first commit

---
+++ src/main/java/com/siddharth/demo/BasicAuthenServerApplication.java
@@ -0,0 +1,13 @@
+package com.siddharth.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BasicAuthenServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BasicAuthenServerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/siddharth/demo/BasicAuthenServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.siddharth.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BasicAuthenServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
